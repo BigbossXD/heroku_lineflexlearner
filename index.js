03e9da582eedd999273b1cd65d603a98f83fc90b
BigbossXD <90498413+BigbossXD@users.noreply.github.com>
2021-11-12 15:52:59 +0700

    v7122dddddddwdd2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const line = require("@line/bot-sdk");
 const app = express();
-const cors = require("cors"); 
+const cors = require("cors");
 var path = require("path");
 
 var port = process.env.PORT || 5000;
@@ -42,25 +42,25 @@ const logger = createLogger({
 });
 //Log Write
 
-
 const client = new line.Client({
-    channelAccessToken: "5jrExr1uaAjIpIG64umLo2QFxJ9NXGAGt5vmcQUDD4It9dj5Ye8zK3GU2eAp/wIjEhNMFa83t6v40qL/Ohrcn5RaCCU1bCvddxzohGA7i0PfqrLh/ujOoaCG1ZYl2J0f+OLOcY3e/L5zd/5ZafEUPQdB04t89/1O/w1cDnyilFU=",
-  });
+  channelAccessToken:
+    "5jrExr1uaAjIpIG64umLo2QFxJ9NXGAGt5vmcQUDD4It9dj5Ye8zK3GU2eAp/wIjEhNMFa83t6v40qL/Ohrcn5RaCCU1bCvddxzohGA7i0PfqrLh/ujOoaCG1ZYl2J0f+OLOcY3e/L5zd/5ZafEUPQdB04t89/1O/w1cDnyilFU=",
+});
 
 app.get("/LineWebhook", (req, res) => {
-    const resp = {
-        Message: "Update Order Success",
-        code: "00000",
-      };
-      res.status(200).json(resp);
-  });
+  const resp = {
+    Message: "Update Order Success",
+    code: "00000",
+  };
+  res.status(200).json(resp);
+});
 
 app.post("/LineWebhook", async (req, res) => {
-//   req.body.events; // webhook event objects
-//   req.body.destination; // user ID of the bot (optional)
- console.log(req.body);
-console.log(JSON.stringify(req.body));
-console.log(JSON.stringify(req.body, null, 2));
+  //   req.body.events; // webhook event objects
+  //   req.body.destination; // user ID of the bot (optional)
+  console.log(req.body);
+  console.log(JSON.stringify(req.body));
+  console.log(JSON.stringify(req.body, null, 2));
   let reply_token = req.body.events[0].replyToken;
   let msg = req.body.events[0].message.text;
 
@@ -94,296 +94,295 @@ console.log(JSON.stringify(req.body, null, 2));
 });
 
 const ReplyMSG = (ReplyToken, message) => {
-    logger.info("This in ReplyMSG Function . . .");
-    client
-      .replyMessage(ReplyToken, message)
-      .then(() => {
-        logger.info("Reply Completed!!!");
-      })
-      .catch((err) => {
-        logger.info(err);
-      });
-  };
+  logger.info("This in ReplyMSG Function . . .");
+  client
+    .replyMessage(ReplyToken, message)
+    .then(() => {
+      logger.info("Reply Completed!!!");
+    })
+    .catch((err) => {
+      logger.info(err);
+    });
+};
 
-  const ReplyFlex1 = (ReplyToken) => {
-    logger.info("This in ReplyMSG Function . . .");
-    const message = {
-        "type": "flex",
-        "altText": "this is a flex message",
-        "contents": {
-          "type": "bubble",
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "text",
-                "text": "hello"
-              },
-              {
-                "type": "text",
-                "text": "world"
-              }
-            ]
-          }
-        }
-      };
-    client
-      .replyMessage(ReplyToken, message)
-      .then(() => {
-        logger.info("Reply Completed!!!");
-      })
-      .catch((err) => {
-        logger.info(err);
-      });
-  };
-
-
-  const ReplyFlex2 = (ReplyToken) => {
-    logger.info("This in ReplyMSG Function . . .");
-    const message = {
-        "type": "flex",
-        "altText": "Q1. อยากดู FLex แบบไหน?",
-        "contents": {
-          "type": "bubble",
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "spacing": "md",
-            "contents": [
-              {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "Q1",
-                    "align": "center",
-                    "size": "xxl",
-                    "weight": "bold"
-                  },
-                  {
-                    "type": "text",
-                    "text": "Q1. อยากดู FLex แบบไหน?",
-                    "wrap": true,
-                    "weight": "bold",
-                    "margin": "lg"
-                  }
-                ]
-              },
-              {
-                "type": "separator"
-              },
-              {
-                "type": "box",
-                "layout": "vertical",
-                "margin": "lg",
-                "contents": [
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "1.",
-                        "flex": 1,
-                        "size": "lg",
-                        "weight": "bold",
-                        "color": "#666666"
-                      },
-                      {
-                        "type": "text",
-                        "text": "Flex bubble",
-                        "wrap": true,
-                        "flex": 9
-                      }
-                    ]
-                  },
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "2.",
-                        "flex": 1,
-                        "size": "lg",
-                        "weight": "bold",
-                        "color": "#666666"
-                      },
-                      {
-                        "type": "text",
-                        "text": "Carousel",
-                        "wrap": true,
-                        "flex": 9
-                      }
-                    ]
-                  },
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "3.",
-                        "flex": 1,
-                        "size": "lg",
-                        "weight": "bold",
-                        "color": "#666666"
-                      },
-                      {
-                        "type": "text",
-                        "text": "Component",
-                        "wrap": true,
-                        "flex": 9
-                      }
-                    ]
-                  },
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "4.",
-                        "flex": 1,
-                        "size": "lg",
-                        "weight": "bold",
-                        "color": "#666666"
-                      },
-                      {
-                        "type": "text",
-                        "text": "flex อื่นๆ",
-                        "wrap": true,
-                        "flex": 9
-                      }
-                    ]
-                  }
-                ]
-              }
-            ]
+const ReplyFlex1 = (ReplyToken) => {
+  logger.info("This in ReplyMSG Function . . .");
+  const message = {
+    type: "flex",
+    altText: "this is a flex message",
+    contents: {
+      type: "bubble",
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          {
+            type: "text",
+            text: "hello",
           },
-          "footer": {
-            "type": "box",
-            "layout": "horizontal",
-            "spacing": "sm",
-            "contents": [
-              {
-                "type": "button",
-                "style": "primary",
-                "height": "sm",
-                "action": {
-                  "type": "message",
-                  "label": "1",
-                  "text": "Flex_Bubble"
-                }
-              },
-              {
-                "type": "button",
-                "style": "primary",
-                "height": "sm",
-                "action": {
-                  "type": "message",
-                  "label": "2",
-                  "text": "Flex_Carousel"
-                }
-              },
-              {
-                "type": "button",
-                "style": "primary",
-                "height": "sm",
-                "action": {
-                  "type": "message",
-                  "label": "3",
-                  "text": "Flex_Component"
-                }
-              },
-              {
-                "type": "button",
-                "style": "primary",
-                "height": "sm",
-                "action": {
-                  "type": "message",
-                  "label": "4",
-                  "text": "Flex_More"
-                }
-              }
-            ]
-          }
-        }
-      };
-    client
-      .replyMessage(ReplyToken, message)
-      .then(() => {
-        logger.info("Reply Completed!!!");
-      })
-      .catch((err) => {
-        logger.info(err);
-      });
+          {
+            type: "text",
+            text: "world",
+          },
+        ],
+      },
+    },
   };
+  client
+    .replyMessage(ReplyToken, message)
+    .then(() => {
+      logger.info("Reply Completed!!!");
+    })
+    .catch((err) => {
+      logger.info(err);
+    });
+};
 
-  ReplyFlex_Flex_Bubble = (ReplyToken) => {
-    logger.info("This in ReplyMSG Function . . .");
-    const message = {
-        "type": "bubble",
-        "direction": "ltr",
-        "header": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "text",
-              "text": "This easy bubble",
-              "align": "center",
-              "contents": []
-            }
-          ]
-        },
-        "hero": {
-          "type": "image",
-          "url": "https://vos.line-scdn.net/bot-designer-template-images/bot-designer-icon.png",
-          "size": "full",
-          "aspectRatio": "1.51:1",
-          "aspectMode": "fit"
-        },
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "text",
-              "text": "Body",
-              "align": "center",
-              "contents": []
-            }
-          ]
-        },
-        "footer": {
-          "type": "box",
-          "layout": "horizontal",
-          "contents": [
-            {
-              "type": "button",
-              "action": {
-                "type": "uri",
-                "label": "Button",
-                "uri": "https://linecorp.com"
-              }
-            }
-          ]
-        }
-      };
-    client
-      .replyMessage(ReplyToken, message)
-      .then(() => {
-        logger.info("Reply Completed!!!");
-      })
-      .catch((err) => {
-        logger.info(err);
-      });
+const ReplyFlex2 = (ReplyToken) => {
+  logger.info("This in ReplyMSG Function . . .");
+  const message = {
+    type: "flex",
+    altText: "Q1. อยากดู FLex แบบไหน?",
+    contents: {
+      type: "bubble",
+      body: {
+        type: "box",
+        layout: "vertical",
+        spacing: "md",
+        contents: [
+          {
+            type: "box",
+            layout: "vertical",
+            contents: [
+              {
+                type: "text",
+                text: "Q1",
+                align: "center",
+                size: "xxl",
+                weight: "bold",
+              },
+              {
+                type: "text",
+                text: "Q1. อยากดู FLex แบบไหน?",
+                wrap: true,
+                weight: "bold",
+                margin: "lg",
+              },
+            ],
+          },
+          {
+            type: "separator",
+          },
+          {
+            type: "box",
+            layout: "vertical",
+            margin: "lg",
+            contents: [
+              {
+                type: "box",
+                layout: "baseline",
+                contents: [
+                  {
+                    type: "text",
+                    text: "1.",
+                    flex: 1,
+                    size: "lg",
+                    weight: "bold",
+                    color: "#666666",
+                  },
+                  {
+                    type: "text",
+                    text: "Flex bubble",
+                    wrap: true,
+                    flex: 9,
+                  },
+                ],
+              },
+              {
+                type: "box",
+                layout: "baseline",
+                contents: [
+                  {
+                    type: "text",
+                    text: "2.",
+                    flex: 1,
+                    size: "lg",
+                    weight: "bold",
+                    color: "#666666",
+                  },
+                  {
+                    type: "text",
+                    text: "Carousel",
+                    wrap: true,
+                    flex: 9,
+                  },
+                ],
+              },
+              {
+                type: "box",
+                layout: "baseline",
+                contents: [
+                  {
+                    type: "text",
+                    text: "3.",
+                    flex: 1,
+                    size: "lg",
+                    weight: "bold",
+                    color: "#666666",
+                  },
+                  {
+                    type: "text",
+                    text: "Component",
+                    wrap: true,
+                    flex: 9,
+                  },
+                ],
+              },
+              {
+                type: "box",
+                layout: "baseline",
+                contents: [
+                  {
+                    type: "text",
+                    text: "4.",
+                    flex: 1,
+                    size: "lg",
+                    weight: "bold",
+                    color: "#666666",
+                  },
+                  {
+                    type: "text",
+                    text: "flex อื่นๆ",
+                    wrap: true,
+                    flex: 9,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      footer: {
+        type: "box",
+        layout: "horizontal",
+        spacing: "sm",
+        contents: [
+          {
+            type: "button",
+            style: "primary",
+            height: "sm",
+            action: {
+              type: "message",
+              label: "1",
+              text: "Flex_Bubble",
+            },
+          },
+          {
+            type: "button",
+            style: "primary",
+            height: "sm",
+            action: {
+              type: "message",
+              label: "2",
+              text: "Flex_Carousel",
+            },
+          },
+          {
+            type: "button",
+            style: "primary",
+            height: "sm",
+            action: {
+              type: "message",
+              label: "3",
+              text: "Flex_Component",
+            },
+          },
+          {
+            type: "button",
+            style: "primary",
+            height: "sm",
+            action: {
+              type: "message",
+              label: "4",
+              text: "Flex_More",
+            },
+          },
+        ],
+      },
+    },
   };
+  client
+    .replyMessage(ReplyToken, message)
+    .then(() => {
+      logger.info("Reply Completed!!!");
+    })
+    .catch((err) => {
+      logger.info(err);
+    });
+};
 
-
+ReplyFlex_Flex_Bubble = (ReplyToken) => {
+  logger.info("This in ReplyMSG Function . . .");
+  const message = {
+    type: "bubble",
+    direction: "ltr",
+    header: {
+      type: "box",
+      layout: "vertical",
+      contents: [
+        {
+          type: "text",
+          text: "This easy bubble",
+          align: "center",
+          contents: [],
+        },
+      ],
+    },
+    hero: {
+      type: "image",
+      url: "https://vos.line-scdn.net/bot-designer-template-images/bot-designer-icon.png",
+      size: "full",
+      aspectRatio: "1.51:1",
+      aspectMode: "fit",
+    },
+    body: {
+      type: "box",
+      layout: "vertical",
+      contents: [
+        {
+          type: "text",
+          text: "Body",
+          align: "center",
+          contents: [],
+        },
+      ],
+    },
+    footer: {
+      type: "box",
+      layout: "horizontal",
+      contents: [
+        {
+          type: "button",
+          action: {
+            type: "uri",
+            label: "Button",
+            uri: "https://linecorp.com",
+          },
+        },
+      ],
+    },
+  };
+  client
+    .replyMessage(ReplyToken, message)
+    .then(() => {
+      console.log("Reply Completed!!!");
+      logger.info("Reply Completed!!!");
+    })
+    .catch((err) => {
+      console.log("err=>" + err.Message + err.message);
+      logger.info(err);
+    });
+};
 
 app.listen(port, () => {
   console.log("server listen on port", port);
