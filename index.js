2d9dfb6b89013d747ee7ee28bea561882ef5a249
BigbossXD <90498413+BigbossXD@users.noreply.github.com>
2021-11-12 16:03:19 +0700

    v7122dddddddwdd2

--- index.js
+++ index.js
@@ -89,6 +89,11 @@ app.post("/LineWebhook", async (req, res) => {
     logger.info("ReplyMSG Msg Outgoing . . . ");
     res.status(200).send("EVENT_RECEIVED");
   }
+  if (msg == "Flex_Carousel") {
+    await ReplyFlex_Flex_Carousel(reply_token);
+    logger.info("ReplyMSG Msg Outgoing . . . ");
+    res.status(200).send("EVENT_RECEIVED");
+  }
 
   logger.info("Webhook End!!");
 });
@@ -387,6 +392,209 @@ ReplyFlex_Flex_Bubble = (ReplyToken) => {
       logger.info(err);
     });
 };
+
+
+ReplyFlex_Flex_Carousel = (ReplyToken) => {
+    logger.info("This in ReplyMSG Function . . .");
+    const message = {
+      type: "flex",
+      altText: "Ez bubble",
+      contents: {
+        "type": "carousel",
+        "contents": [
+          {
+            "type": "bubble",
+            "hero": {
+              "type": "image",
+              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
+              "size": "full",
+              "aspectRatio": "20:13",
+              "aspectMode": "cover"
+            },
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "Arm Chair, White",
+                  "weight": "bold",
+                  "size": "xl",
+                  "wrap": true,
+                  "contents": []
+                },
+                {
+                  "type": "box",
+                  "layout": "baseline",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "$49",
+                      "weight": "bold",
+                      "size": "xl",
+                      "flex": 0,
+                      "wrap": true,
+                      "contents": []
+                    },
+                    {
+                      "type": "text",
+                      "text": ".99",
+                      "weight": "bold",
+                      "size": "sm",
+                      "flex": 0,
+                      "wrap": true,
+                      "contents": []
+                    }
+                  ]
+                }
+              ]
+            },
+            "footer": {
+              "type": "box",
+              "layout": "vertical",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "button",
+                  "action": {
+                    "type": "uri",
+                    "label": "Add to Cart",
+                    "uri": "https://linecorp.com"
+                  },
+                  "style": "primary"
+                },
+                {
+                  "type": "button",
+                  "action": {
+                    "type": "uri",
+                    "label": "Add to wishlist",
+                    "uri": "https://linecorp.com"
+                  }
+                }
+              ]
+            }
+          },
+          {
+            "type": "bubble",
+            "hero": {
+              "type": "image",
+              "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png",
+              "size": "full",
+              "aspectRatio": "20:13",
+              "aspectMode": "cover"
+            },
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "Metal Desk Lamp",
+                  "weight": "bold",
+                  "size": "xl",
+                  "wrap": true,
+                  "contents": []
+                },
+                {
+                  "type": "box",
+                  "layout": "baseline",
+                  "flex": 1,
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "$11",
+                      "weight": "bold",
+                      "size": "xl",
+                      "flex": 0,
+                      "wrap": true,
+                      "contents": []
+                    },
+                    {
+                      "type": "text",
+                      "text": ".99",
+                      "weight": "bold",
+                      "size": "sm",
+                      "flex": 0,
+                      "wrap": true,
+                      "contents": []
+                    }
+                  ]
+                },
+                {
+                  "type": "text",
+                  "text": "Temporarily out of stock",
+                  "size": "xxs",
+                  "color": "#FF5551",
+                  "flex": 0,
+                  "margin": "md",
+                  "wrap": true,
+                  "contents": []
+                }
+              ]
+            },
+            "footer": {
+              "type": "box",
+              "layout": "vertical",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "button",
+                  "action": {
+                    "type": "uri",
+                    "label": "Add to Cart",
+                    "uri": "https://linecorp.com"
+                  },
+                  "flex": 2,
+                  "color": "#AAAAAA",
+                  "style": "primary"
+                },
+                {
+                  "type": "button",
+                  "action": {
+                    "type": "uri",
+                    "label": "Add to wish list",
+                    "uri": "https://linecorp.com"
+                  }
+                }
+              ]
+            }
+          },
+          {
+            "type": "bubble",
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "spacing": "sm",
+              "contents": [
+                {
+                  "type": "button",
+                  "action": {
+                    "type": "uri",
+                    "label": "See more",
+                    "uri": "https://linecorp.com"
+                  },
+                  "flex": 1,
+                  "gravity": "center"
+                }
+              ]
+            }
+          }
+        ]
+      }
+    };
+    client
+      .replyMessage(ReplyToken, message)
+      .then(() => {
+        console.log("Reply Completed!!!");
+        logger.info("Reply Completed!!!");
+      })
+      .catch((err) => {
+        console.log("err=>" + err.Message + err.message);
+        logger.info(err);
+      });
+  };
 
 app.listen(port, () => {
   console.log("server listen on port", port);
